b35ce2c24de4252d84940c960d7ab6a8283a02f0
Maraysa <maraysa1515@gmail.com>
2021-12-13 16:38:12 -0300

    Criando a primeira posicao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace Jogo_de_Xadrez___Console
 {
@@ -6,7 +7,9 @@ namespace Jogo_de_Xadrez___Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
         }
     }
 }
