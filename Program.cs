9ad2bcf8c07f84b3225c7749bd7c0dd7ff9d4370
Maraysa <maraysa1515@gmail.com>
2021-12-13 16:22:18 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jogo_de_Xadrez___Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
