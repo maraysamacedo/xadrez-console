4621188e9588fbd487ec3f7298ce3f3bea3399b3
Maraysa <maraysa1515@gmail.com>
2021-12-14 10:20:13 -0300

    Posicao xadrez

--- Program.cs
+++ Program.cs
@@ -8,22 +8,28 @@ namespace Jogo_de_Xadrez___Console
     {
         static void Main(string[] args)
         {
-            try
-            {
+            /*try
+             {
 
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                 Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
 
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            Console.ReadLine();
+                 Tela.imprimirTabuleiro(tab);
+             }
+             catch (TabuleiroException e)
+             {
+                 Console.WriteLine(e.Message);
+             }*/
+
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+
+            Console.WriteLine(pos);
+
+
+            Console.WriteLine(pos.toPosicao());
         }
     }
 }
