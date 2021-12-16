8db743da3d0804251ad1ab659bb3b2fd1917ffdd
Maraysa <maraysa1515@gmail.com>
2021-12-16 17:30:44 -0300

    Jogada especial: Promocao

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -142,6 +142,21 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em Xeque!");
             }
+
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial Promocao
+            if (p is Peao) {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque (adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -159,7 +174,7 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             //#jogadaespecial En Passant
 
